5667d5c60864ff7955f1af6ce5a95668706e3692
shawncrawley <shawncrawley@gmail.com>
2016-11-17 23:54:57 -0700

    added time stamp and url links

--- tethysapp/data_rods_explorer/tests/unit_tests.py
+++ tethysapp/data_rods_explorer/tests/unit_tests.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from tethysapp.data_rods_explorer.model_objects import init_model, get_datarods_tsb, get_var_dict, get_model_fences
-from datetime import datetime
+from datetime import datetime, timedelta
 import urllib2
 
 
 def test_nasa_endpoints(request):
+    start = datetime.now()
     init_model()
     model_fences = get_model_fences()
     datarods_tsb = get_datarods_tsb()
@@ -22,8 +23,13 @@ def test_nasa_endpoints(request):
 
     for model in model_fences:
         nasa_url_template = datarods_tsb[model]
-        start_date = datetime.strptime(model_fences[model]['start_date'], '%m/%d/%Y').strftime('%Y-%m-%d') + 'T23'
-        end_date = datetime.strptime(model_fences[model]['end_date'], '%m/%d/%Y').strftime('%Y-%m-%d') + 'T00'
+        # Use if testing range of all available dates
+        # start_date = datetime.strptime(model_fences[model]['start_date'], '%m/%d/%Y').strftime('%Y-%m-%d') + 'T23'
+        # end_date = datetime.strptime(model_fences[model]['end_date'], '%m/%d/%Y').strftime('%Y-%m-%d') + 'T00'
+        # Use if specifying a smaller subset of available dates from latest date backwards
+        end_date_obj = datetime.strptime(model_fences[model]['end_date'], '%m/%d/%Y')
+        end_date = end_date_obj.strftime('%Y-%m-%d') + 'T00'
+        start_date = (end_date_obj - timedelta(days=7)).strftime('%Y-%m-%d') + 'T00'
         extents = model_fences[model]['extents']
         y = float(extents['minY']) + ((float(extents['maxY']) - float(extents['minY'])) / 2)
         x = float(extents['minX']) + ((float(extents['maxX']) - float(extents['minX'])) / 2)
@@ -44,17 +50,32 @@ def test_nasa_endpoints(request):
                 error += 1
                 failed_urls.append(url)
 
-    results = '''
-    TESTS COMPLETE!
-    TOTAL ENDPOINTS TESTED: {0}
-    TOTAL SUCCESSFUL: {1}
-    TOTAL FAILS: {2}
-    {3}{4}
-    '''.format(counter, success, error,
-               'FAILED ENDPOINTS: ' if len(failed_urls) > 0 else '',
-               '\n'.join(failed_urls))
+    end = datetime.now()
+    elapsed = end - start
 
-    print results
-    context = {'results': '<br>'.join(results.split('\n'))}
+    results_text = '''
+    TESTS COMPLETE!
+
+    SUMMARY:
+
+        START TIME: {0}
+        END TIME: {1}
+        ELAPSED TIME: {2}
+        TOTAL ENDPOINTS TESTED: {3}
+        TOTAL SUCCESSFUL: {4}
+        TOTAL FAILS: {5}
+    '''.format(start, end, elapsed, counter, success, error)
+
+    if failed_urls:
+        console_results = results_text + '\n\tFAILED ENDPOINTS:\n\t' + '\n\t'.join(failed_urls)
+    else:
+        console_results = results_text
+
+    print console_results
+
+    context = {
+        'results_text': '<br>'.join(results_text.split('\n')),
+        'failed_urls': failed_urls
+    }
 
     return render(request, 'data_rods_explorer/test-results.html', context)
