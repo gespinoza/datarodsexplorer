9340928d12eaf0a3a7712d8d64e8f165b547c8db
henrichsen <41309272+henrichsen@users.noreply.github.com>
2020-07-23 11:38:08 -0600

    Tethys 3 update

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-import os
-import sys
-from setuptools import setup, find_packages
-from tethys_apps.app_installation import custom_develop_command, custom_install_command
+from setuptools import setup, find_namespace_packages
+from tethys_apps.app_installation import find_resource_files
 
 ### Apps Definition ###
 app_package = 'data_rods_explorer'
 release_package = 'tethysapp-' + app_package
-app_class = 'data_rods_explorer.app:DataRodsExplorer'
-app_package_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tethysapp', app_package)
 
 ### Python Dependencies ###
 dependencies = ['python-dateutil', 'bs4']
+
+# -- Get Resource File -- #
+resource_files = find_resource_files('tethysapp/' + app_package + '/templates', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/public', 'tethysapp/' + app_package)
+resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces', 'tethysapp/' + app_package)
+
 
 setup(
     name=release_package,
@@ -22,13 +24,9 @@ setup(
     author_email='gespinoza@utexas.edu',
     url='',
     license='',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['tethysapp', 'tethysapp.' + app_package],
+    packages=find_namespace_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
     install_requires=dependencies,
-    cmdclass={
-        'install': custom_install_command(app_package, app_package_dir, dependencies),
-        'develop': custom_develop_command(app_package, app_package_dir, dependencies)
-    }
+
 )
