0047c50a6133d2d92977a9334df152484fec3386
henrichsen <41309272+henrichsen@users.noreply.github.com>
2021-03-10 14:25:44 -0700

    Update Version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ resource_files += find_resource_files('tethysapp/' + app_package + '/workspaces'
 
 setup(
     name=release_package,
-    version='0.0.1',
+    version='0.0.2',
     description='Plot and visualize data from LDAS',
     long_description='',
     keywords='',
